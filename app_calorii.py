f2f785738c3ec0efa499632f42c0ddfd90e4a667
Andreea Gurzu <123237227+anndreeaaa@users.noreply.github.com>
2023-04-04 18:24:38 +0300

    Add files via upload

---
+++ app_calorii.py
@@ -0,0 +1,144 @@
+
+'''
+print("Bun venit la calculatorul de calorii zilnice!")
+sex = input("Introduceti sexul (M/F): ")
+varsta = int(input("Introduceti varsta: "))
+inaltime = float(input("Introduceti inaltimea (cm): "))
+greutate = float(input("Introduceti greutatea (kg): "))
+
+
+alimente = {
+    "avocado": 160,
+    "banane": 89,
+    "broccoli": 34,
+    "cartofi": 86,
+    "cereale integrale": 350,
+    "ciocolata neagra": 174,
+    "fasole": 333,
+    "grapefruit": 42,
+    "iaurt": 60,
+    "lapte": 58,
+    "mar": 52,
+    "morcovi": 41,
+    "nuci": 550,
+    "ovaz": 389,
+    "paine": 250,
+    "pepene verde": 30,
+    "porumb dulce": 86,
+    "pui": 165, 
+    "ridichi": 16, 
+    "rosii": 18, 
+    "sfecla rosie": 43, 
+    "somon": 206, 
+    "spanac": 23, 
+    "tofu": 76, 
+    "ulei de masline": 884, 
+    "usturoi": 149, 
+    "vin": 85,
+    "somon": 127               
+}
+
+
+# Funcția pentru calcularea valorii calorice pentru un aliment
+def calculeaza_calorii(aliment, cantitate):
+    if aliment in alimente:
+        calorii = alimente[aliment] * cantitate / 100
+        return calorii
+    else:
+        return None
+
+
+
+
+total_calorii = 0
+
+alimente_consumate = []
+
+
+while True:
+    aliment = input("Introduceti un aliment consumat sau tastati 'gata' pentru a opri: ")
+    if aliment == "gata":
+        break
+    if aliment not in alimente:
+        print("Alimentul introdus nu exista in baza de date.")
+        continue
+    total_calorii += alimente[aliment]
+    alimente_consumate.append(aliment)
+
+print("Nr total de calorii consumate este: ", total_calorii)
+
+print("Alimentele consumate sunt: ", alimente_consumate)
+'''
+
+
+
+#api retete
+
+print("Bun venit la calculatorul de calorii zilnice!")
+sex = input("Introduceti sexul (M/F): ")
+varsta = int(input("Introduceti varsta: "))
+inaltime = float(input("Introduceti inaltimea (cm): "))
+greutate = float(input("Introduceti greutatea (kg): "))
+
+
+# Lista de alimente și valorile calorice
+alimente = {
+    "avocado": 160,
+    "banane": 89,
+    "broccoli": 34,
+    "cartofi": 86,
+    "cereale integrale": 350,
+    "ciocolata neagra": 174,
+    "fasole": 333,
+    "grapefruit": 42,
+    "iaurt": 60,
+    "lapte": 58,
+    "mar": 52,
+    "morcovi": 41,
+    "nuci": 550,
+    "ovaz": 389,
+    "paine": 250,
+    "pepene verde": 30,
+    "porumb dulce": 86,
+    "pui": 165, 
+    "ridichi": 16, 
+    "rosii": 18, 
+    "sfecla rosie": 43, 
+    "somon": 206, 
+    "spanac": 23, 
+    "tofu": 76, 
+    "ulei de masline": 884, 
+    "usturoi": 149, 
+    "vin": 85,
+    "somon": 127               
+}
+
+
+# Functia pentru calcularea valorii calorice pentru un aliment
+def calculeaza_calorii(aliment, cantitate):
+    if aliment in alimente:
+        calorii = alimente[aliment] * cantitate / 100
+        return calorii
+    else:
+        return None
+
+# Functia pentru calcularea valorii calorice pentru o lista de alimente
+def calculeaza_total_calorii(lista_alimente):
+    total_calorii = 0
+    for aliment, cantitate in lista_alimente:
+        calorii = calculeaza_calorii(aliment, cantitate)
+        if calorii:
+            total_calorii += calorii
+    return total_calorii
+
+# Solicita alimentele și cantitățile utilizatorului și afișează totalul de calorii
+lista_alimente = []
+while True:
+    aliment = input("Introduceți un aliment sau apăsați ENTER pentru a finaliza: ")
+    if not aliment:
+        break
+    cantitate = float(input("Introduceți cantitatea în grame: "))
+    lista_alimente.append((aliment, cantitate))
+
+total_calorii = calculeaza_total_calorii(lista_alimente)
+print("Total calorii:", total_calorii)
